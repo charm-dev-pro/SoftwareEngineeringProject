8138c39ecfdc9a8a678871ec496bfdd05bcd9091
james <James.y.kang@uscga.edu>
2025-04-03 14:30:27 -0400

    learned to add words

--- WeekView.py
+++ WeekView.py
@@ -7,41 +7,45 @@ from tkinter import ttk
 import tkinter.messagebox
 from tkinter import *
 
-conn = sqlite3.connect('SEProject1.db')
-cur = conn.cursor()
-
-
 class MonthView():
 
     # CREATE MAIN gui window
-    root = tk.Tk()
-    canvas = Canvas(root, bg="white", height = 1500, width = 1300)
+    root = Tk()
     root.title("Week View")
-    #(root, height, width, bd, bg, ..) https://www.geeksforgeeks.org/python-tkinter-canvas-widget/
-    verticalLine1 = canvas.create_line(186, 0, 186, 1000, width=5)
-    verticalLine2 = canvas.create_line(372, 0, 372, 1000, width=5)
-    verticalLine3 = canvas.create_line(558, 0, 558, 1000, width=5)
-    verticalLine4 = canvas.create_line(744, 0, 744, 1000, width=5)
-    verticalLine5 = canvas.create_line(930, 0, 930, 1000, width=5)
-    verticalLine6 = canvas.create_line(1110, 0, 1110, 1000, width=5)
 
-    horizontalLine1 = canvas.create_line(0, 50, 1300, 50, width=5)
+    #(root, height, width, bd, bg, ..) https://www.geeksforgeeks.org/python-tkinter-canvas-widget/
+    # verticalLine1 = root.create_line(186, 0, 186, 1000, width=5)
+    # verticalLine2 = root.create_line(372, 0, 372, 1000, width=5)
+    # verticalLine3 = root.create_line(558, 0, 558, 1000, width=5)
+    # verticalLine4 = root.create_line(744, 0, 744, 1000, width=5)
+    # verticalLine5 = root.create_line(930, 0, 930, 1000, width=5)
+    # verticalLine6 = root.create_line(1110, 0, 1110, 1000, width=5)
+    #https://www.tutorialspoint.com/how-to-set-a-certain-number-of-rows-and-columns-of-a-tkinter-grid
+    #https://pythonlobby.com/python-grid-grid-geometry-manager-in-tkinter-grid-layout/
+    label1 = Label(root, text='Label1', font=("Calibri, 15"))
+    label1.grid(column=0, row=0)
+    label2 = Label(root, text='Label2', font=("Calibri, 15"))
+    label2.grid(column=1, row=1)
+    label = Label(root, text='Label2', font=("Calibri, 15"))
+    label.grid(column=1000, row=2, rowspan = 5)
+    w = Label(root, text="I'm in 3rd Column", bg="red", fg="white")
+    w.grid(column=3)
+    w = Label(root, text="I've columnspan of 3", bg="light green", fg="white")
+    w.grid(columnspan=3)
+    w = Label(root, text="I've ipadx of 5", bg="yellow", fg="red")
+    w.grid(ipadx=100)
+    # horizontalLine1 = root.create_line(0, 50, 1300, 50, width=5)
 #https://www.geeksforgeeks.org/python-tkinter-label/
 
-    l = Label(root, text="asdf")
-    l.config(padx=20, pady=20, font=("Courier", 28))
-
+    # label1 = Label(root, text='Label1', font=("Calibri, 15"))
+    # label1.grid(column=0, row=0)
     #https: // www.geeksforgeeks.org / python - tkinter - scrollbar /
 
     # create and config and form frame
-    form_frame = tk.Frame(root, padx=20, pady=20)
-    form_frame.pack(padx=20, pady=20, fill=tk.BOTH, expand=True)
-    text_box = Text(root,height=13,width=32,font=12)
 
 # implement a scroll thingy later https://pythonguides.com/python-tkinter-scrollbar/
 
     # run tkinter main loop
-    l.pack()
-
-    canvas.pack()
+    #https://stackoverflow.com/questions/7966119/display-fullscreen-mode-on-tkinter
+    root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
     root.mainloop()
